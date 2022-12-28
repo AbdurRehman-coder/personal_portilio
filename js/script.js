9932c42b2b76b30933cc1f1883851789c0b9a25b
Nabeel Ahmad <nab34923@gmail.com>
2022-12-29 02:22:01 +0500

    Personal Portfolio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Typing Animation
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Developer .",
+    "ReactJS Developer .",
+    "React-Native Developer .",
+    "MERN-Stack Developer .",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
